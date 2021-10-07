1af4cd666b2fb1b3a177db02f002d53eedc0d863
DiracMD <ninggujxiao@gmail.com>
2021-10-07 12:02:01 +0800

    add eigen-3.4.0

--- Practice-for-Cplus/Pro1/readmol_new.cc
+++ Practice-for-Cplus/Pro1/readmol_new.cc
@@ -1,14 +1,25 @@
 // read the data
 #include "molecule.h"
+#include "masses.h"
+
 #include <iostream> // the input and ouptput; the cin and cout, and cerr
 #include <fstream>  // both funtcion from the ofstream and ifstream 
 #include <iomanip>
 #include <cstdio>
 #include <cmath>
 
+#include "Eigen/Dense"
+#include "Eigen/Eigenvalues"
+#include "Eigen/Core"
+
+typedef Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>Matrix;
+//this code defines a new type called Matrix that may be dynamically allocated and contains only doubles
+typedef Eigen::Matrix<double, Eigen::Dynamic, 1> Vector;
+
+
 using namespace std;
 
-int main(int argc, char *argv[])
+int main()
 {
     Molecule mol("acetaldehyde.dat",0);
     cout << "Number of atoms: " << mol.natom << endl;
@@ -56,5 +67,62 @@ int main(int argc, char *argv[])
             }
         }
     }
+    /*find the center of mass*/
+    double M=0.0;
+    for(int i=0;i<mol.natom;i++) M+=an2masses[(int) mol.zvals[i]];
+
+    double xcm=0.0;
+    double ycm=0.0;
+    double zcm=0.0;
+    double mi;
+    for(int i=0;i<mol.natom;i++) {
+        mi=an2masses[(int) mol.zvals[i]];
+        xcm +=mi*mol.geom[i][0];
+        ycm +=mi*mol.geom[i][1];
+        zcm +=mi*mol.geom[i][2];
+    }
+    xcm /=M;
+    ycm /=M;
+    zcm /=M;
+    printf("\n Molecular center of mass: %12.8f %12.8f %12.8f\n",xcm,ycm,zcm);
+
+    mol.translate(-xcm,-ycm,-zcm);
+
+    Matrix I(3,3);
+
+    for(int i=0;i<mol.natom;i++) {
+        mi = an2masses[(int) mol.zvals[i]];
+        I(0,0) += mi*(mol.geom[i][1]*mol.geom[i][1]+mol.geom[i][2]*mol.geom[i][2]);
+        I(1,1) += mi*(mol.geom[i][0]*mol.geom[i][0]+mol.geom[i][2]*mol.geom[i][2]);
+        I(2,2) += mi*(mol.geom[i][1]*mol.geom[i][1]+mol.geom[i][0]*mol.geom[i][0]);
+
+        I(0,1) -= mi*mol.geom[i][0]*mol.geom[i][1];
+        I(0,2) -= mi*mol.geom[i][0]*mol.geom[i][2];
+        I(0,2) -= mi*mol.geom[i][1]*mol.geom[i][2];
+
+    }
+    I(1,0)=I(0,1);
+    I(2,0)=I(0,2);
+    I(2,1)=I(1,2);
+
+    cout << "\nMoment of interia tensor (amu bohr^2): \n";
+    cout << I << endl;
+
+    // find the principle moments
+    Eigen::SelfAdjointEigenSolver<Matrix> solver(I);
+    Matrix evecs = solver.eigenvectors();
+    Matrix evals = solver.eigenvalues();
+
+    cout << "\nPrinciples moments of inertia (amu*bohr^2):\n";
+    cout << evals <<endl;
+
+    double conv=0.529177249*0.529177249;
+    cout << "\nPrinciples moments of inertia (amu*AA^2):\n";
+    cout << evals*conv <<endl;
+
+    double conv1=0.529177249E-8*0.529177249E-8*1.6605402E-24;
+    cout << "\nPrinciples moments of inertia (g*cm^2):\n";
+    cout << evals*conv1 <<endl;
+
     return 0;
 }
